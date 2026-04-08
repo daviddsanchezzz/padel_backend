9e8e79f27da2a252bf28b92529ff67158d9d8f65
DavidSanchez287761 <287761@student@pwr.edu.pl>
2026-04-08 14:08:01 +0200

    fff

--- src/index.js
+++ src/index.js
@@ -13,7 +13,11 @@ const standingsRoutes   = require('./routes/standings.routes');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth',         authRoutes);
